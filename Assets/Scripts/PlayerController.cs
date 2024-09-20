67b9abd32638fc64c02ec621576b889fecc4c995
JaeYoun Kim <rlawodusx@naver.com>
2024-09-20 20:01:43 +0900

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float playerYPos;    // 플레이어의 position 저장 
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 초기의 플레이어의 위치 저장
+        playerYPos = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        // 마우스 왼쪽 클릭시 혹은 모바일에서 터치할 경우 
+        if (Input.GetMouseButtonDown(0))
+        {
+            // player position 변경
+
+
+        }
+    }
+}
